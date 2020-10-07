0d1d207993582095af26dcdf596c0d18b98d04a5
Mitch Gilbert <mitchellpgilbert@gmail.com>
2020-10-07 23:59:28 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
     array.length.times do |index| 
       new_array.push(array[index] * array[index])
     end
-  put new_array
+  puts new_array
 end
 
 square_array([9,10,11])
