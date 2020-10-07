06f37ca400f882f64d67fbfad7eddf1fd29e1f16
Mitch Gilbert <mitchellpgilbert@gmail.com>
2020-10-07 23:59:51 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+  new_array = [] 
+    array.length.times do |index| 
+      new_array.push(array[index] * array[index])
+    end
+  new_array
 end
+
+square_array([9,10,11])
